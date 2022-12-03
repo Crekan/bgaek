657c39cbd65522c7b5e59f6d76a405ada87e7179
vaniakazeko <vania.kazeko.00@bk.ru>
2022-12-03 11:21:01 +0300

    fix back file

--- back/back/settings.py
+++ back/back/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bpz*%vyg4v(xc06!gei$iwtbvz95ozl_)&w+ve(5eb#^1wh!oi'
+SECRET_KEY = 'django-insecure-0k%kpklhb1f29ru!zu#po2qo%y&fg+#_9na%5yq+ss%%l%+)rz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
